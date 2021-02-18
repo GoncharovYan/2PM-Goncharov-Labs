cadf761b4e16b0c9622658bd37a5138d1e469dba
WhatDuckWhat <66702619+WhatDuckWhat@users.noreply.github.com>
2021-02-18 19:43:58 +0200

    Add files via upload

---
+++ AiPO/Lab3/Lab3a/Lab3a/Source.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool find_up_connection(vector<vector<char>> vec, int i, int& j, int m) {
+	bool connect = false;
+	while (vec[i][j] == '#') {
+		if (vec[i - 1][j] == '#')
+			connect = true;
+		j++;
+
+		if (j == m)	return connect;
+	}
+
+	return connect;
+}
+
+/*
+#####
+#...#
+#...#
+#...#
+#####
+
+0
+
+*/
+
+void find_side_connection(vector<vector<char>> vec, int i, int m, int& count) {
+	int m_count = 0; //Number of connected gardens
+	int swt = 0;
+
+	for (int j = 0; j < m; j++) {
+
+		swt = 0;
+		if (vec[i][j] == '#')								swt = 1;
+		if (vec[i][j] == '#' && vec[i - 1][j] == '#')		swt = 2;
+// #.#
+// #.#
+// ###
+//
+// m_count = 1;
+// count = 2;
+		switch (swt)
+		{
+		case 1:
+		{
+			while (j < m && vec[i][j] == '#' && vec[i - 1][j] != '#')
+				j++;
+			j--;
+			break;
+		}
+		case 2:
+		{
+			while (j < m && vec[i][j] == '#' && vec[i - 1][j] == '#')
+				j++;
+			m_count++;
+			j--;
+			break;
+		}
+		default:
+		{
+			if (m_count > 1) count -= m_count - 1;
+			m_count = 0;
+			break;
+		}
+		}
+
+	}
+
+	if (swt != 0 && m_count > 1) count -= m_count - 1;
+	return;
+}
+
+
+
+
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<vector<char>> vec(n, vector<char>(m));
+
+	for (auto& el : vec) {
+		for (auto& ell : el) {
+			cin >> ell;
+		}
+	}
+
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+
+			if (i == 0) {
+
+				if (vec[i][j] == '#') {
+					while (vec[i][j] == '#') {
+						j++;
+						if (j == m)
+							break;
+					}
+					count++;
+				}
+			}
+			else {
+				if (vec[i][j] == '#')
+					if (!find_up_connection(vec, i, j, m)) count++;
+			}
+		}
+		if (i != 0)	find_side_connection(vec, i, m, count);
+	}
+
+	std::cout << endl;
+	for (auto& el : vec) {
+		for (auto& ell : el) {
+			std::cout << ell;
+		}
+		std::cout << endl;
+	}
+	std::cout << endl << count << endl;
+	return 0;
+}
---
+++ AiPO/Lab3/Lab3b/Lab3b/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	int QueCount; //Count of questions
+	cin >> QueCount;
+
+	map<int, bool> Ans;
+	int tmp;
+	for (int i = 0; i < QueCount; i++) { //Writing the correct answers to the tree
+		cin >> tmp;
+		Ans.insert(pair<int, bool>(tmp, true));
+	}
+
+	int StudsCount; //Count of studs
+	cin >> StudsCount;
+
+	int StudAnswers; //Count of studs answers
+	int CorrectAnswers = 0; //Count of studs correct answers
+	int answer;
+
+	for (int i = 0; i < StudsCount; i++) { //Each student
+		cin >> StudAnswers; //Enter the count of stud answers
+		for (int i = 0; i < StudAnswers; i++) { 
+			cin >> answer; //Enter the i answer
+			if (Ans[answer])
+				CorrectAnswers++;
+		}
+		cout << CorrectAnswers << endl;
+		CorrectAnswers = 0;
+	}
+	
+	return 0;
+}
