94e09e2708f730610a116c7ec1dbdedc13a119b2
WhatDuckWhat <66702619+WhatDuckWhat@users.noreply.github.com>
2021-02-18 15:13:24 +0200

    Add files via upload

---
+++ AiPO/Lab1/Lab1b/Lab1b/Source.cpp
@@ -0,0 +1,33 @@
+﻿/*
+2)Последовательность
+Вывести ряд Фибоначчи до n-го символа.
+При выполнении задания использовать массив, объявленный как F[NMAX], где NMAX - целая,
+положительная, числовая константа(100, 200, 5).
+Формула ряда:
+F(0)=0, F(1) =1, F(n)=F(n-1)+F(n-2)
+*/
+#include <iostream>
+using namespace std;
+
+void Fib(long long F[], const int N)
+{
+	if (N > 1) {
+		F[1] = 1;
+		for (int i = 2; i < N; i++)
+			F[i] = F[i - 2] + F[i - 1];
+
+	}
+}
+
+int main()
+{
+	const int NMAX(50);
+	long long F[NMAX] = {};
+
+	Fib(F, NMAX);
+
+	for (auto el : F)
+		cout << el << endl;
+
+	return 0;
+}
---
+++ AiPO/Lab1/Lab1c/Lab1c/Source.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+unsigned long long int next = 1;
+
+void srand100(unsigned int seed)
+{
+	next = seed;
+}
+
+int rand100() { //Линейный конгруэнтный метод
+	next = next * 1103515245 + 12345;
+	return next % 100;
+}
+
+long double doublerand() {
+	next = next * 1103515245 + 12345;
+	long long dbl = next % 1000000000000;
+	long double tdbl = static_cast< long double>(dbl) / 1000000000000;
+	return tdbl;
+}
+
+int main()
+{
+	std::cout << std::setprecision(12);
+	srand100(time(NULL));
+	int a = rand100();
+	std::cout << "Rand from 0 to 100: " << a << std::endl;
+
+	long double b = doublerand();
+	std::cout << "Rand from 0 to 1 with a tail of 12 rand characters: " << b << std::endl;
+
+	std::cout << std::endl << "Rand100 test: " << std::endl;
+	for (int i = 0; i < 20; i++)
+		std::cout << rand100() << std::endl;
+	
+	std::cout << std::endl << "Doublerand test: " << std::endl;
+	for (int i = 0; i < 20; i++)
+		std::cout << doublerand() << std::endl;
+
+	return 0;
+}
